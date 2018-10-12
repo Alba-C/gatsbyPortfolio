9f91ee16c23efdc6a2ae6da9839bf632925e5d82
Chris Albanese <Christopher@CafeRE.com>
2018-10-11 23:40:20 -0400

    updated gatsby-plugin-manifest options with new icon img

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,13 +14,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Christopher Albanese | Front End Developer',
+        short_name: 'Albanese Portfolio',
         start_url: '/',
         background_color: '#283044',
         theme_color: '#283044',
         display: 'minimal-ui',
-        icon: '/assets/CA-Logo.png', // This path is relative to the root of the site.
+        icon: 'public/icons/icon-144x144.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
