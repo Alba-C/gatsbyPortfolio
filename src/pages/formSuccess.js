56dbd5d59cd488a2f9c802511556e61373afbf04
Chris Albanese <Christopher@CafeRE.com>
2018-10-16 10:51:59 -0400

    updated base text color, new resume.pdf added img to success

--- src/pages/formSuccess.js
+++ src/pages/formSuccess.js
@@ -8,19 +8,22 @@ const FormSuccess = () => (
       className={css`
         background-color: #f1f7ed;
         text-align: center;
-        color: black;
+        color: #002851;
         height: 83vh;
         padding: 50px;
+        background-image: url('/static/assets/pugInJacket.jpg');
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        h1 {
+          @media (max-width: 600px) {
+           font-size: 1.5rem;
+          }
+        }
       `}
     >
-      <h1>Thank You, Your message was sent</h1>
-      <a href="/">
-      back to home
-      </a>
-
-      
-
-     
+      <h1>Thank You, Your message was sent.</h1>
+      <a href="/">back to home</a>
     </section>
   </Layout>
 )
