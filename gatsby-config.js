9025f64eaae99a364aa3efb375ab074dc1790bca
Chris Albanese <Christopher@CafeRE.com>
2018-10-18 10:17:19 -0400

    move GA trackingId to head

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
       options: {
         trackingId: 'UA-127667741-1',
         // Puts tracking script in the head instead of the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
