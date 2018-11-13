d00aff4b1a5917a1f0da2b0913e5b330763addbb
Chris Albanese <Christopher@CafeRE.com>
2018-11-12 23:52:40 -0500

    add custom recaptcha

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { css } from 'emotion'
+import ReCAPTCHA from 'react-google-recaptcha'
+
+// const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY
 
 const form = css({
   fontSize: '1.25rem',
@@ -13,232 +16,180 @@ const form = css({
   },
 })
 
-const Contact = () => {
-  return (
-    <section
-      className={css`
-        background-color: #c0e0de;
-      `}
-    >
-      <div
-        className={css`
-          max-width: 1200px;
-          margin: 0 auto;
-          background-color: #c0e0de;
-          font-size: 1.15em;
-          display: flex;
-          flex-direction: column;
-          padding: 25px 50px 50px 50px;
-        `}
-        id="contact"
-      >
-        <h2
-          className={css`
-            text-align: center;
-          `}
-        >
-          Contact Me
-        </h2>
-        <p
-          className={css`
-            text-align: center;
-            font-weight: bold;
-          `}
-        >
-          Do you want to hire me? Need a consultation on a new project?
-        </p>
-        <p>
-          Perhaps, you need assistance moving a large sum of money
-          internationally and require someone who won't ask a lot of questions.
-          ¯\_(ツ)_/¯ Whatever the reason, don't be shy. I'd love to hear from
-          you.
-        </p>
-        <form
-          name="contactForm"
-          method="POST"
-          action="/formSuccess"
-          data-netlify="true"
-          netlify-honeypot="bot-field"
-          className={css`
-            font-size: 1rem;
+
+
+export default class Contact extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      
+    }
+  }
+
+  handleRecaptcha = value => {
+  this.setState({ 'g-recaptcha-response': value })
+  }
+  
+  form = css({
+    fontSize: '1.25rem',
+    overflow: 'hidden',
+    backgroundColor: 'transparent',
+    boxShadow: '0 0 10px 0 black',
+    zIndex: 999,
+    display: 'flex',
+    '@media (max-width: 600px)': {
+      display: 'block',
+    },
+  })
+  render() {
+ 
+    return <section className={css`
+          background-color: #c0e0de;`}>
+        <div className={css`
+            max-width: 1200px;
+            margin: 0 auto;
+            background-color: #c0e0de;
+            font-size: 1.15em;
             display: flex;
             flex-direction: column;
-            justify-content: stretch;
-            background-color: #f1f7ed;
-            padding: 30px;
-            margin: 0 -15px;
-            border-radius: 5px;
-          `}
-        >
-          <input type="hidden" name="contactForm" value="contactForm" />
-          <p
-            className={css`
+            padding: 25px 50px 50px 50px;`} id="contact">
+          <h2 className={css`
+              text-align: center;`}>
+            Contact Me
+          </h2>
+          <p className={css`
+              text-align: center;
+              font-weight: bold;`}>
+            Do you want to hire me? Need a consultation on a new project?
+          </p>
+          <p>
+            Perhaps, you need assistance moving a large sum of money
+            internationally and require someone who won't ask a lot of
+            questions. ¯\_(ツ)_/¯ Whatever the reason, don't be shy. I'd
+            love to hear from you.
+          </p>
+          <form name="contactForm" method="POST" action="/formSuccess" data-netlify="true" netlify-honeypot="bot-field" data-netlify-recaptcha="true" className={css`
+              font-size: 1rem;
               display: flex;
-              justify-content: center;
-              flex-wrap: wrap;
-              @media (max-width: 535px) {
-                justify-content: flex-start;
-              }
-            `}
-          >
-            <label
-              htmlFor="name"
-              className={css`
-                width: 115px;
-              `}
-            >
-              Your Name:
-            </label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              required
-              placeholder=" your name"
-              className={css`
-                width: 70%;
-                margin-left: 15px;
-                border-radius: 5px;
-                @media (max-width: 535px) {
-                  width: 90%;
-                  margin-left: 0;
-                }
-              `}
-            />
-          </p>
-          <p
-            className={css`
-              display: flex;
-              justify-content: center;
-              flex-wrap: wrap;
-              @media (max-width: 535px) {
-                justify-content: flex-start;
-              }
-            `}
-          >
-            <label
-              htmlFor="email"
-              className={css`
-                width: 115px;
-              `}
-            >
-              Your Email:
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              required
-              placeholder=" your@email.com"
-              className={css`
-                width: 70%;
-                margin-left: 15px;
-                border-radius: 5px;
-                @media (max-width: 535px) {
-                  width: 90%;
-                  margin-left: 0;
-                }
-              `}
-            />
-          </p>
+              flex-direction: column;
+              justify-content: stretch;
+              background-color: #f1f7ed;
+              padding: 30px;
+              margin: 0 -15px;
+              border-radius: 5px;`}>
+            <input type="hidden" name="contactForm" value="contactForm" />
+            <p className={css`
+                display: flex;
+                justify-content: center;
+                flex-wrap: wrap;
+                @media (max-width: 535px) {justify-content: flex-start;}`}>
+              <label htmlFor="name" className={css`
+                  width: 115px;`}>
+                Your Name:
+              </label>
+              <input type="text" name="name" id="name" required placeholder=" your name" className={css`
+                  width: 70%;
+                  margin-left: 15px;
+                  border-radius: 5px;
+                  @media (max-width: 535px) {width: 90%;
+                    margin-left: 0;}`} />
+            </p>
+            <p className={css`
+                display: flex;
+                justify-content: center;
+                flex-wrap: wrap;
+                @media (max-width: 535px) {justify-content: flex-start;}`}>
+              <label htmlFor="email" className={css`
+                  width: 115px;`}>
+                Your Email:
+              </label>
+              <input type="email" name="email" id="email" required placeholder=" your@email.com" className={css`
+                  width: 70%;
+                  margin-left: 15px;
+                  border-radius: 5px;
+                  @media (max-width: 535px) {width: 90%;
+                    margin-left: 0;}`} />
+            </p>
 
-          <p
-            className={css`
-              display: flex;
-              justify-content: center;
-              flex-wrap: wrap;
-              @media (max-width: 535px) {
-                justify-content: flex-start;
-              }
-            `}
-          >
-            <label
-              htmlFor="message"
-              className={css`
-                width: 115px;
-              `}
-            >
-              Message:
-            </label>
-            <textarea
-              name="message"
-              id="message"
-              placeholder="Let me know how I can help!"
-              className={css`
-                border-radius: 5px;
-                margin-left: 15px;
-                width: 70%;
-                min-height: 10vw;
-                @media (max-width: 535px) {
-                  width: 90%;
-                  height: 50vw;
-                  margin-left: 0;
-                }
-              `}
-            />
-          </p>
-          <p
-            className={css`
-              visibility: hidden;
-              position: absolute;
-            `}
-          >
-            <label htmlFor="bot-field">
-              Don’t fill this out if you're human:{' '}
-              <input name="bot-field" id="bot-field" />
-            </label>
-          </p>
+            <p className={css`
+                display: flex;
+                justify-content: center;
+                flex-wrap: wrap;
+                @media (max-width: 535px) {justify-content: flex-start;}`}>
+              <label htmlFor="message" className={css`
+                  width: 115px;`}>
+                Message:
+              </label>
+              <textarea name="message" id="message" placeholder="Let me know how I can help!" className={css`
+                  border-radius: 5px;
+                  margin-left: 15px;
+                  width: 70%;
+                  min-height: 10vw;
+                  @media (max-width: 535px) {width: 90%;
+                    height: 50vw;
+                    margin-left: 0;}`} />
+            </p>
+            <p className={css`
+                visibility: hidden;
+                position: absolute;`}>
+              <label htmlFor="bot-field">
+                Don’t fill this out if you're human: <input name="bot-field" id="bot-field" />
+              </label>
+            </p>
 
-          <div netlify-recaptcha data-netlify-recaptcha />
+            <div className={css`
+                display: flex;
+                justify-content: center;
+                flex-wrap: wrap;
+               
+                @media (max-width: 535px) {justify-content: flex-start;}`}>
+              <span className={css`
+                  width: 115px;
+                  
+                  @media (max-width: 564px) {display: none;}`} />
+              <span className={css`
+                  width: 70%;
+                  margin-left: 15px;
+                  border-radius: 5px;
+                  @media (max-width: 535px) {width: 90%;
+                    margin-left: 0;}`}>
+                <ReCAPTCHA ref="recaptcha" sitekey="6LdSXHoUAAAAAEtEK_b6AMI8gXK2sLNLZRVqWTNh" onChange={this.handleRecaptcha} />
+              </span>
+            </div>
 
-          <p
-            className={css`
-              display: flex;
-              justify-content: center;
-              flex-wrap: wrap;
-              @media (max-width: 535px) {
-                justify-content: flex-start;
-              }
-            `}
-          >
-            <span
-              className={css`
-                width: 70%;
-                @media (max-width: 564px) {
-                  display: none;
-                }
-              `}
-            />
+            <p className={css`
+                display: flex;
+                justify-content: center;
+                flex-wrap: wrap;
+                @media (max-width: 535px) {justify-content: flex-start;}`}>
+              <span className={css`
+                  width: 70%;
+                  @media (max-width: 564px) {display: none;}`} />
 
-            <button
-              type="submit"
-              className={css`
-                width: 130px;
-                border-radius: 5px;
-                background-color: #bada55;
-                color: rgb(69, 69, 69);
-                font-size: 1rem;
-                font-weight: 800;
-                box-shadow: 0px 1px 1px 0 rgba(69, 69, 69, 0.439);
-                cursor: pointer;
-                transition: all 0.3s ease 0s;
-                &:hover {
-                  background-color: #82d8d8;
-                }
-                @media (max-width: 563px) {
-                  /* width: 70%; */
+            <button type="submit" className={css`
+                  margin-top: 15px;
+                  width: 130px;
+                  border-radius: 5px;
+                  background-color: #bada55;
+                  color: rgb(69, 69, 69);
+                  font-size: 1rem;
+                  font-weight: 800;
+                  box-shadow: 0px 1px 1px 0 rgba(69, 69, 69, 0.439);
+                  cursor: pointer;
+                  transition: all 0.3s ease 0s;
+                  &:hover {
+                    background-color: #82d8d8;
+                  }
+                  @media (max-width: 563px) {/* width: 70%; */
 
-                  /* margin: 0 auto; */
-                }
-              `}
-            >
-              Send It
-            </button>
-          </p>
-        </form>
-      </div>
-    </section>
-  )
+                    /* margin: 0 auto; */}`}>
+                Send It
+              </button>
+            </p>
+          </form>
+        </div>
+      </section>
+  }
 }
 
-export default Contact
+
