1814fb6b68043b753c695bd4fa7ead155c512068
Chris Albanese <Christopher@CafeRE.com>
2021-07-17 17:45:00 -0400

    refactor navbar and footer

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,6 @@
-import { Component } from 'react';
-import { jsx } from '@emotion/react';
+import React, { Component } from 'react';
+import { css } from '@emotion/react';
 import { Link, withPrefix } from 'gatsby';
-/** @jsx jsx */
 
 class Navbar extends Component {
   constructor(props) {
@@ -35,209 +34,167 @@ class Navbar extends Component {
   }
 
   render() {
-    const topnav = {
-      fontSize: '1.25rem',
-      overflow: 'hidden',
-      backgroundColor: 'transparent',
-      zIndex: 999,
-      display: 'flex',
-      transition: 'height 0.2s ease-in-out',
-      '@media (max-width: 768px)': {
-        position: 'fixed',
-        flexDirection: 'column',
-        top: 0,
-        left: 0,
-        right: 0,
-        backgroundColor: '#002851',
-        height: 60,
-        overflow: 'visible'
+    const topNavStyles = css`
+      font-size: 1.25rem;
+      overflow: hidden;
+      background-color: transparent;
+      z-index: 999;
+      display: flex;
+      transition: height 0.2s ease-in-out;
+      @media (max-width: 768px) {
+        position: fixed;
+        flex-direction: column;
+        top: 0;
+        left: 0;
+        right: 0;
+        background-color: #002851;
+        height: 60px;
+        overflow: visible;
       }
-    };
 
-    const navBrand = {
-      color: '#f2f2f2',
-      textAlign: 'center',
-      padding: '14px 16px',
-      textDecoration: 'none',
-      '&:hover': {
-        textDecoration: 'none',
-        color: this.state.collapsed ? 'inherit' : '#bada55',
-        borderBottom: this.state.collapsed ? 'none' : '3px solid #bada55'
-      },
-      '@media (max-width: 768px)': {
-        margin: 'auto',
-        position: 'absolute',
-        top: 10,
-        padding: 0,
-        marginLeft: 5,
-        height: 45,
-        width: 45
+      .nav__brand-container {
+        color: #f2f2f2;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        &:hover {
+          text-decoration: none;
+          color: ${this.state.collapsed ? 'inherit' : '#bada55'};
+          border-bottom: ${this.state.collapsed ? 'none' : '3px solid #bada55'};
+        }
+        @media (max-width: 768px) {
+          margin: auto;
+          position: absolute;
+          top: 10px;
+          padding: 0;
+          margin-left: 5px;
+          height: 45px;
+          width: 45px;
+        }
       }
-    };
 
-    const logo = {
-      marginBottom: 0,
-      height: 'auto',
-      maxHeight: '60px',
-      width: 'auto'
-    };
-
-    const linkContainer = {
-      maxWidth: 1000,
-      margin: '12px auto',
-      height: '100%',
-      paddingRight: 88,
-      transition: 'all 0.3s ease-in-out',
-      '@media (max-width: 768px)': {
-        backgroundColor: '#002851',
-        flexDirection: 'column',
-        minHeight: '100vh',
-        paddingTop: 60,
-        margin: '0 0 0 auto',
-        width: 400,
-        maxWidth: '70%',
-        transform: this.state.collapsed ? 'translateX(400px)' : 'translateX(0)'
+      .nav__brand {
+        margin-bottom: 0;
+        height: auto;
+        max-height: 60px;
+        width: auto;
       }
-    };
 
-    const menuBtn = {
-      float: 'right',
-      padding: '10px 10px',
-      textDecoration: 'none',
-      display: 'none',
-      border: 'none',
-      backgroundColor: 'transparent',
-      '&:hover': {},
-      '@media (max-width: 768px)': {
-        display: 'block',
-        position: 'absolute',
-        top: 0,
-        right: 0
-      }
-    };
+      .nav__link-container {
+        max-width: 1000px;
+        margin: 12px auto;
+        height: 100%;
+        padding-right: 88px;
+        transition: all 0.3s ease-in-out;
+        @media (max-width: 768px) {
+          background-color: #002851;
+          flex-direction: column;
+          min-height: 100vh;
+          padding-top: 60px;
+          margin: 0 0 0 auto;
+          width: 400px;
+          max-width: 70%;
+          transform: ${this.state.collapsed ? 'translateX(400px)' : 'translateX(0)'};
+        }
 
-    const topnavA = {
-      float: 'left',
-      display: 'block',
-      color: '#f2f2f2',
-      textAlign: 'center',
-      padding: '14px 16px',
-      textDecoration: 'none',
-      transition: 'all 0.2s ease-in-out',
-      borderBottom: '3px solid transparent',
-      '&:hover': {
-        textDecoration: 'none',
-        color: '#bada55',
-        borderColor: '#bada55',
-        fontWeight: 'bold'
-      },
-      '@media (max-width: 768px)': {
-        float: 'none',
-        textAlign: 'left'
+        .nav__link {
+          float: left;
+          display: block;
+          color: #f2f2f2;
+          text-align: center;
+          padding: 14px 16px;
+          text-decoration: none;
+          transition: all 0.2s ease-in-out;
+          border-bottom: 3px solid transparent;
+          &:hover {
+            text-decoration: none;
+            color: #bada55;
+            border-color: #bada55;
+            font-weight: bold;
+          }
+          @media (max-width: 768px) {
+            float: none;
+            text-align: left;
+          }
+        }
       }
-    };
+    `;
 
     return (
-      <nav css={topnav} id="myTopnav">
-        <Link to="/" onClick={this.closeMenu} css={navBrand}>
-          <img src={withPrefix('assets/images/CA-Logo.png')} alt="logo" css={logo} />
+      <nav css={topNavStyles} id="myTopnav">
+        <Link className="nav__brand-container" to="/" onClick={this.closeMenu}>
+          <img className="nav__brand" src={withPrefix('assets/images/CA-Logo.png')} alt="logo" />
         </Link>
-        <div css={linkContainer}>
-          <a href="/#projects" css={topnavA} onClick={this.closeMenu}>
+        <div className="nav__link-container">
+          <a className="nav__link" href="/#projects" onClick={this.closeMenu}>
             Projects
           </a>
-          <a href="/#about" css={topnavA} onClick={this.closeMenu}>
+          <a className="nav__link" href="/#about" onClick={this.closeMenu}>
             About
           </a>
-          <a href="/#contact" css={topnavA} onClick={this.closeMenu}>
+          <a className="nav__link" href="/#contact" onClick={this.closeMenu}>
             Contact
           </a>
-          <Link to="/resume" css={topnavA} onClick={this.closeMenu}>
+          <Link className="nav__link" to="/resume" onClick={this.closeMenu}>
             Resume
           </Link>
         </div>
-        <button css={menuBtn} aria-label="menu toggle" onClick={this.handleClick}>
-          <Hamburger collapsed={this.state.collapsed} size={40} lineColor={'white'} />
-        </button>
+        <Hamburger collapsed={this.state.collapsed} size={40} lineColor={'white'} onClick={this.handleClick} />
       </nav>
     );
   }
 }
 
-class Hamburger extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collapsed: this.props.collapsed
-    };
-  }
+const Hamburger = ({ collapsed, background, lineColor, size, onClick }) => {
+  const burgerStyles = css`
+    height: ${size}px;
+    width: ${size}px;
+    display: block;
+    background-color: ${background};
+    border: solid 1px ${lineColor} !important;
+    padding: 0;
+    border-radius: ${collapsed ? '10%' : '50%'};
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    transition: border-radius 0.3s ease-in;
+    background-color: transparent;
 
-  background = this.props.background;
-  lineColor = this.props.lineColor;
-  size = this.props.size;
+    .burger__line {
+      display: block;
+      position: absolute;
+      background-color: ${lineColor};
+      margin: 0 10%;
+      height: 2px;
+      width: 80%;
+      transform-origin: left;
+      transition: all 0.3s ease-in;
+    }
 
-  // use this to get the button click from the navBar component
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (nextProps.collapsed !== prevState.collapsed) {
-      return { collapsed: nextProps.collapsed };
-    } else return null;
-  }
+    .burger__line--top {
+      top: ${collapsed ? '15%' : `50%`};
+      transform: ${collapsed ? 'rotate(0deg)' : 'translate(5px , -12px) rotate(45deg)'};
+    }
 
-  render() {
-    const menu = {
-      height: this.size,
-      width: this.size,
-      backgroundColor: this.background,
-      border: `solid 1px ${this.lineColor}`,
-      borderRadius: this.state.collapsed ? '10%' : '50%',
-      position: 'relative',
-      transition: 'border-radius .3s ease-in'
-    };
+    .burger__line--middle {
+      transform-origin: center;
+      transform: translateY(-50%) ${collapsed ? 'rotate(0deg)' : 'rotate(-45deg)'};
+    }
 
-    const lineTop = {
-      position: 'absolute',
-      backgroundColor: this.lineColor,
-      // backgroundColor: 'limegreen',
-      margin: '0 10%',
-      top: this.state.collapsed ? '15%' : `50%`,
-      height: 2,
-      width: '80%',
-      transformOrigin: 'left',
-      transform: this.state.collapsed ? 'rotate(0deg)' : 'translate(5px , -12px) rotate(45deg)',
-      transition: 'transform .3s ease-in, top .3s ease-in'
-    };
+    .burger__line--bottom {
+      top: unset;
+      bottom: 15%;
+      transform: ${collapsed ? 'rotate(0deg)' : 'translate(5px , -2px) rotate(-45deg)'};
+    }
+  `;
 
-    const lineMid = {
-      position: 'absolute',
-      margin: '0 10%',
-      top: '50%',
-      transform: 'translateY(-50%)',
-      backgroundColor: this.lineColor,
-      height: 2,
-      transformOrigin: 'right',
-      width: this.state.collapsed ? '80%' : 0
-      // transition: 'width .4s ease-in .1s',
-    };
-
-    const lineLast = {
-      position: 'absolute',
-      backgroundColor: this.lineColor,
-      height: 2,
-      margin: '0 10%',
-      bottom: '15%',
-      width: '80%',
-      transformOrigin: 'left',
-      transform: this.state.collapsed ? 'rotate(0deg)' : 'translate(5px , -2px) rotate(-45deg)',
-      transition: 'transform .3s ease-in, top .3s ease-in'
-    };
-    return (
-      <div css={menu}>
-        <div css={lineTop} />
-        <div css={lineMid} />
-        <div css={lineLast} />
-      </div>
-    );
-  }
-}
+  return (
+    <button css={burgerStyles} type="button" onClick={onClick}>
+      <span className="burger__line burger__line--top" />
+      <span className="burger__line burger__line--middle" />
+      <span className="burger__line burger__line--bottom" />
+    </button>
+  );
+};
 
 export default Navbar;
